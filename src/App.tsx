d95c30b54f63936a92cec6dfe8d094a0c16399c5
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-22 07:39:24 -0300

    aula-02-complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons { 
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 export function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
   return (
-    <div>
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-        })}
-      </ul>
-    </div>
+   <Event />
   )
 }
 
