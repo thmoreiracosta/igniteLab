a0e2c79849ba5da5242866845be20c16802df234
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-25 20:57:19 -0300

    Projeto finalizado - subscribed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
 
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <a href="./">
+      <a href="/">
         <Logo />
       </a>
     </header>
